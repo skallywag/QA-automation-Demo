c2d511506a46f459107f6be1b3b4605420721806
Samuel Lee <lee.sam1492@gmail.com>
2022-12-02 13:47:02 -0700

    change

--- qa-demo/src/App.js
+++ qa-demo/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1 id="pageTitle">QA Automation Demo Github CI</h1>
+        <h1 id="pageTitle">QA Automation Demo Github</h1>
         <a id="gcLink" target="blank" href="https://www.greencompassdev.com">
           Link to GC
         </a>
