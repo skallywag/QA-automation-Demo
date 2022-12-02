bb63540ccb006f28b2e56b6ad907bc10c34d9109
Samuel Lee <lee.sam1492@gmail.com>
2022-12-02 13:34:59 -0700

    changed test method

--- qa-demo/tests/example.spec.js
+++ qa-demo/tests/example.spec.js
@@ -1,11 +1,20 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("homepage title is correct", async ({ page }) => {
-  await page.goto("localhost:3000");
+// test("homepage title is correct", async ({ page }) => {
+//   await page.goto("localhost:3000");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/React App/);
-  let pageTitle = await page.locator("#pageTitle");
-  await expect(pageTitle).toHaveText("QA Automation Demo Github CI");
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/React App/);
+//   let pageTitle = await page.locator("#pageTitle");
+//   await expect(pageTitle).toHaveText("QA Automation Demo Github CI");
+// });
+
+test("Join Now Route", async ({ page }) => {
+  await page.goto("https://www.greencompassdev.com/");
+  await page.getByRole("link", { name: "LOGIN" }).click();
+  await page.getByRole("link", { name: "Join Now" }).click();
+  await expect(page).toHaveURL(
+    "https://www.greencompassdev.com/enrollment/become-an-associate"
+  );
 });
