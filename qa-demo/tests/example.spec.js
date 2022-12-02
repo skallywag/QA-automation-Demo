b4f91db0ae78b9febc5dc6f7b43ec6d1d1bca7e1
Samuel Lee <lee.sam1492@gmail.com>
2022-12-02 13:55:41 -0700

    chjange

--- qa-demo/tests/example.spec.js
+++ qa-demo/tests/example.spec.js
@@ -1,20 +1,20 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-// test("homepage title is correct", async ({ page }) => {
-//   await page.goto("localhost:3000");
-
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/React App/);
-//   let pageTitle = await page.locator("#pageTitle");
-//   await expect(pageTitle).toHaveText("QA Automation Demo Github CI");
-// });
-
-test("Join Now Route", async ({ page }) => {
+test("homepage title is correct", async ({ page }) => {
   await page.goto("https://www.greencompassdev.com/");
-  await page.getByRole("link", { name: "LOGIN" }).click();
-  await page.getByRole("link", { name: "Join Now" }).click();
-  await expect(page).toHaveURL(
-    "https://www.greencompassdev.com/enrollment/become-an-associate"
-  );
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/React App/);
+  let pageTitle = await page.locator("#pageTitle");
+  await expect(pageTitle).toHaveText("QA Automation Demo Github CI");
 });
+
+// test("Join Now Route", async ({ page }) => {
+//   await page.goto("https://www.greencompassdev.com/");
+//   await page.getByRole("link", { name: "LOGIN" }).click();
+//   await page.getByRole("link", { name: "Join Now" }).click();
+//   await expect(page).toHaveURL(
+//     "https://www.greencompassdev.com/enrollment/become-an-associate"
+//   );
+// });
